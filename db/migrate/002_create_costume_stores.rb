316280003daaf913ef62a68178b19dfd31b479cc
Kristen Peck <kpeck71@gmail.com>
2017-11-29 12:52:57 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,9 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :price
-      t.string :size
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.boolean :size
       t.string :image_url
     end
   end
