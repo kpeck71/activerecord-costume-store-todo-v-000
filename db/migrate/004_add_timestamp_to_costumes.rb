20b8f77a4fbe9fea0b84560679807f01bc1bae09
Kristen Peck <kpeck71@gmail.com>
2017-11-29 13:03:47 +0000

    automatically backed up by learn

--- db/migrate/004_add_timestamp_to_costumes.rb
+++ db/migrate/004_add_timestamp_to_costumes.rb
@@ -1,4 +1,4 @@
-class AddTimestampToCreateCostumes < ActiveRecord::Migration[4.2]
+class AddTimestampToCostumes < ActiveRecord::Migration[4.2]
   def change
     add_column :costumes, :timestamp
   end
