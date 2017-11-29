1dfa6328a28ff125fb93401916a07e251ecedfde
Kristen Peck <kpeck71@gmail.com>
2017-11-29 12:55:45 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,8 +1,8 @@
-# name
-# location
-# theme
-# price
-# whether they're family friendly or not
-# opening date
-# closing date
-# long description
+#t.string :name
+#t.string :location
+#t.string :theme
+#t.string :price
+# t.boolean :family friendly
+# :opening_date
+# :closing_date
+# t.text :long_description
