59824bb9fbdbd0734b6d47f2d2ada0ad49348e81
Kristen Peck <kpeck71@gmail.com>
2017-11-29 12:52:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,6 +2,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
+      t.string :location
       t.integer :price
       t.string :size
       t.string :image_url
